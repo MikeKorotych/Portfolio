1321d401a580dabf55a9945cd17ef53f896aed90
Mike <mixakievchanen@gmail.com>
2022-04-22 10:33:22 +0300

    +

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,18 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+    menu = document.querySelector('.menu'),
+    cloceElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
-closeElem.addEventListener('click', () => {
+cloceElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+const levels = document.querySelectorAll('.level__item-value'),
+    lines = document.querySelectorAll('.level__item-bar-fill');
+
+levels.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
